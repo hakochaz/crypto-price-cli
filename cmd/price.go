4df85ff1eb505bc93beefa002266b004a226097c
Charlie <charlie@Charlies-Mac-mini.local>
2021-06-07 19:11:06 +0100

    getting the current price

--- cmd/price.go
+++ cmd/price.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -26,15 +11,19 @@ import (
 // priceCmd represents the price command
 var priceCmd = &cobra.Command{
 	Use:   "price",
-	Short: "Get current price data",
-	Long:  `Get current price data for any cryptocurrency pair`,
+	Short: "Get current or historical price data",
+	Long:  `Get current or historical price data for any cryptocurrency pair`,
 	Run: func(cmd *cobra.Command, args []string) {
 		coin, _ := cmd.Flags().GetString("coin")
 		vc, _ := cmd.Flags().GetString("vc")
+		d, _ := cmd.Flags().GetString("date")
+
 		if coin == "" || vc == "" {
 			fmt.Println("vc and coin are required flags.")
+		} else if d != "" {
+
 		} else {
-			pd := getPriceData(coin, vc)
+			pd := getCurrentPriceData(coin, vc)
 			fmt.Println("Current Price Data")
 			fmt.Println("Coin: ", pd.coin)
 			fmt.Println("Currency: ", pd.vc)
@@ -45,17 +34,7 @@ var priceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(priceCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	priceCmd.PersistentFlags().String("coin", "", "The coin you wish to get the price")
-	priceCmd.PersistentFlags().String("vc", "", "The currency to compare to")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// priceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	priceCmd.PersistentFlags().String("date", "", "The date and time you wish to get data for")
 }
 
 type PriceData struct {
@@ -64,7 +43,9 @@ type PriceData struct {
 	price float32
 }
 
-func getPriceData(coin, vc string) PriceData {
+// getCurrentPriceData gets current price data for a coin
+// versus another currency
+func getCurrentPriceData(coin, vc string) PriceData {
 	pd := PriceData{}
 	cg := gecko.NewClient(nil)
 	sp, err := cg.SimplePrice([]string{coin}, []string{vc})
--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/hakochaz/crypto-price-cli
 go 1.16
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
-	github.com/spf13/viper v1.7.0
 	github.com/superoo7/go-gecko v1.0.0
 )
